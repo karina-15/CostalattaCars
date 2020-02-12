b49a16d16e4691255d2e3a4a2388a7ad81a2d742
Karina Elias <karinaelias55@gmail.com>
2020-02-12 14:10:25 -0800

    Update LoanSummaryActivity.java cleaned up comments

--- app/src/main/java/edu/miracosta/cs134/costalattacars/LoanSummaryActivity.java
+++ app/src/main/java/edu/miracosta/cs134/costalattacars/LoanSummaryActivity.java
@@ -5,13 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
-import java.util.Locale;
-
-import edu.miracosta.cs134.costalattacars.model.CarLoan;
 
 public class LoanSummaryActivity extends AppCompatActivity {
     // Instance variables
